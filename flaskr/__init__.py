6fe011e1659a846526731558a7eb1881076e149e
Michel <mekalissa0@gmail.com>
2020-12-25 15:51:07 +0200

    Update app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -17,7 +17,6 @@ def create_app(test_config=None):
   '''
   @app.after_request
   def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET, POST,PATCH, DELETE, OPTIONS')
     return response
     
